24d915c3b497a8583f33ee350129ce7dc387c816
Josh the Man <gta.alpha.activities@gmail.com>
2025-08-31 21:12:54 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -185,6 +185,7 @@ function App() {
       <Route path="/services/:slug" element={<DynamicServicePage />} />
       <Route path="/service-areas" element={<ServiceAreasPage />} />
       <Route path="/blog" element={<BlogPage />} />
+      <Route path="/blog/:slug" element={<BlogPostPage />} />
       <Route path="/blog" element={<BlogPage />} />
     </Routes>
   );
