30822dbc1d0320ee7638dcb8bc184801ba37fec5
Josh the Man <gta.alpha.activities@gmail.com>
2025-09-07 00:29:11 -0500

    Updated ServiceAreasPage.tsx

--- src/components/ServiceAreasPage.tsx
+++ src/components/ServiceAreasPage.tsx
@@ -15,6 +15,7 @@ export default function ServiceAreasPage() {
     if (state.restorePosition) return;
 
     // Do not scroll here. App.tsx handles default top and special effects.
+  }, [location]);
 
   const CORE_CITIES = [
     "Omaha",
