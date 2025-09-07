ef94980077c4f8d861c5e04bed708e19825395bd
Josh the Man <gta.alpha.activities@gmail.com>
2025-09-07 00:02:50 -0500

    Updated BlogPage.tsx

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -112,8 +112,9 @@ export default function BlogPage() {
 
                   <Link
                     to="/service-areas"
-                    state={{ fromBlog: true, scrollFx: "midThenTop" }}
+                    state={{ fromBlog: true }}
                     onClick={(e) => {
+                      try { sessionStorage.setItem("lastScrollY", String(window.scrollY)); } catch {}
                       trackClick("blog_service_areas_pill_click", e.currentTarget, {
                         source_page: "blog_index",
                         page_section: "header",
