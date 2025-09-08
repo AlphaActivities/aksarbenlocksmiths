7a73e2c1ddbd49157a69e85d7da3d9073697c681
Josh the Man <gta.alpha.activities@gmail.com>
2025-09-07 22:05:40 -0500

    Updated BlogPostPage.tsx

--- src/pages/BlogPostPage.tsx
+++ src/pages/BlogPostPage.tsx
@@ -85,7 +85,6 @@ export default function BlogPostPage() {
             <div className="relative z-10 text-white pt-12 md:pt-14">
               <main className="min-h-screen w-full px-4 py-12 md:py-16">
                 <section className="mx-auto max-w-5xl text-white">
-                  <div className="mb-6 flex items-center justify-between">
                   <div className="mb-6 -ml-[2px] flex items-center justify-between">
                     <button
                       onClick={(e) => {
