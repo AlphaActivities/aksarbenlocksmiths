8b4b062a8d32e6ac009941cc3b8684773b1ffc09
Josh the Man <gta.alpha.activities@gmail.com>
2025-09-25 11:27:35 -0500

    Migrate next 5 posts to per-file structure

--- src/data/posts/index.ts
+++ src/data/posts/index.ts
@@ -8,12 +8,26 @@ import rekeyVsReplace from "./rekey-vs-replace-omaha";
 import masterKeyGretna from "./master-key-systems-for-gretna-shops";
 import mobileBellevue from "./mobile-locksmith-bellevue-time-saver";
 import mythsOmaha from "./top-5-emergency-locksmith-myths-omaha";
+import lockedOutOmaha from "./what-to-do-if-youre-locked-out-in-omaha";
+import rekeyVsReplace from "./rekey-vs-replace-omaha";
+import masterKeyGretna from "./master-key-systems-for-gretna-shops";
+import mobileBellevue from "./mobile-locksmith-bellevue-time-saver";
+import mythsOmaha from "./top-5-emergency-locksmith-myths-omaha";
 
 // For all remaining posts not yet migrated, continue to rely on the legacy array.
 import { BLOG_POSTS as LEGACY_BLOG_POSTS } from "../blogPosts";
 
 // Build the combined list: migrated posts first, then any legacy posts that are not duplicates
 const migrated = [
+  papillionSpareKey,
+  carKeyProgramming,
+  transparentPricing,
+  lockedOutOmaha,
+  rekeyVsReplace,
+  masterKeyGretna,
+  mobileBellevue,
+  mythsOmaha,
+];
   papillionSpareKey,
   carKeyProgramming,
   transparentPricing,
